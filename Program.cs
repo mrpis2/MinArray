d30c72d14bdb9dfbe32fa9756ddc2d63108398ad
mrpis2 <warhammerprogamer@gmail.com>
2023-12-27 22:06:23 +0700

    MinArray

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿class Program
+{
+    private static void Main(string[] args)
+    {
+        int[] array = { 1, 0, 5, 4, 2, 23, 54, 64, 22, 12 };
+        int index = Min(array);
+        Console.WriteLine("Gia tri nho nhat la: " + array[index] + " vi tri so: " + (index+1));
+
+    }
+
+    static int Min(int[] array)
+    {
+        int min = array[0];
+        int index = 1;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] < min)
+            {
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
